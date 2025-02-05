eeabc75bd759d984b3e638e41f3577007e628e0f
pengwenhao <pengwenhao@aliyun.com>
2025-02-05 17:28:43 +0800

    fix:update BasicPrint

--- basic/BasicPrint.py
+++ basic/BasicPrint.py
@@ -87,3 +87,34 @@ print(dict[2])
 print(tinydict)
 print(tinydict.keys())
 print(tinydict.values())
+
+print("---------------------- 算术运算符 ------------------------")
+
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print('c的值为:', c)
+
+c = a - b
+print('c的值为:', c)
+
+c = a * b
+print('c的值为:', c)
+
+c = a % b
+print('c的值为:', c)
+
+a = 2
+b = 3
+c = a ** b
+
+print("c的值为:", c)
+
+a = 10
+b = 5
+
+c = a // b
+
+print('c的值为:', c)
