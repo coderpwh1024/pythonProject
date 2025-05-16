d141b7ee51d9d080e6a4cba7000b0e197248b431
pengwenhao <pengwenhao@aliyun.com>
2025-05-16 18:06:04 +0800

    feat:add mainifest

---
+++ mainifest.py
@@ -0,0 +1,21 @@
+import json
+
+input_file_path = "music.csv"
+output_file_path = "train.json"
+
+converted_data = []
+with open(input_file_path, 'r', encoding='utf-8') as file:
+    for line in file:
+        data = json.loads(line)
+        instruction = data['data']['instruction']
+        for key in data.keys():
+            if key.startswith('label-'):
+                output = data[key]['results'][0]['data']
+                converted_data.append({
+                    'instruction': instruction,
+                    'output': output
+                })
+                break
+
+with open(output_file_path, 'w', encoding='utf-8') as outfile:
+    json.dump(converted_data, outfile, ensure_ascii=False, indent=4)
